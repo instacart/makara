c40af3131908c6a25ebdd59eb92dc74967b62073
Paul Henry & Eric Saxby <pair+paul+sax@wanelo.com>
2012-12-14 12:30:54 -0800

    Adding additional postgres errors.

--- lib/makara/connection/error_handler.rb
+++ lib/makara/connection/error_handler.rb
@@ -44,7 +44,7 @@ module Makara
         message = message.to_s.downcase
 
         case message
-        when /(closed|lost|no)\s?(\w+)? connection/, /gone away/, /connection (not open|is closed)/i, /reset has failed/, /the database system is starting up/
+        when /(closed|lost|no)\s?(\w+)? connection/, /gone away/, /connection (not open|is closed)/i, /reset has failed/, /the database system is starting up/, /no connection to the server/, /could not connect to server/, /result has been cleared/
           true
         else
           false
