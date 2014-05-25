c70154ae7bc916397aa01f9972b3ca8e203a3044
Mike Nelson <mike@mikeonrails.com>
2014-05-25 14:16:26 -0400

    oh yeah, hash rockets

--- spec/connection_wrapper_spec.rb
+++ spec/connection_wrapper_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe Makara::ConnectionWrapper do
 
-  let(:proxy){ FakeProxy.new({:makara => {connections: [{role: 'master'}, {role: 'slave'}, {role: 'slave'}]}}) }
+  let(:proxy){ FakeProxy.new({:makara => {:blacklist_duration => 5, :connections => [{:role => 'master'}, {:role => 'slave'}, {:role => 'slave'}]}}) }
   let(:connection){ subject.__getobj__ }
+
   subject{ proxy.master_pool.connections.first }
 
   it 'should extend the connection with new functionality' do
@@ -17,4 +18,17 @@ describe Makara::ConnectionWrapper do
     connection.execute("test")
   end
 
+  it 'should have a default weight of 1' do
+    expect(subject._makara_weight).to eq(1)
+  end
+
+  it 'should store the blacklist status' do
+    expect(subject._makara_blacklisted?).to eq(false)
+    subject._makara_blacklist!
+    expect(subject._makara_blacklisted?).to eq(true)
+    subject._makara_whitelist!
+    expect(subject._makara_blacklisted?).to eq(false)
+  end
+
+  it 'shou'
 end
