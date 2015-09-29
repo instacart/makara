4dceade5981badec0ed1d18451093e4cfe7aa289
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-09-29 11:02:54 -0500

    Fix url config PR test failures
    Finishes https://github.com/taskrabbit/makara/pull/93

--- lib/makara/config_parser.rb
+++ lib/makara/config_parser.rb
@@ -26,6 +26,114 @@ module Makara
       :sticky => true
     }
 
+    # ConnectionUrlResolver is borrowed from Rails 4-2 since its location and implementation
+    # vary slightly among Rails versions, but the behavior is the same.  Thus, borrowing the
+    # class should be the most future-safe way to parse a database url.
+    #
+    # ActiveRecord::Base::ConnectionSpecification::Resolver.send(:connection_url_to_hash, url_config[:url])
+    # https://github.com/rails/rails/blob/3-2-stable/activerecord/lib/active_record/connection_adapters/abstract/connection_specification.rb#L60-L77
+    # ActiveRecord::ConnectionAdapters::ConnectionSpecification::Resolver.send(:connection_url_to_hash, url_config[:url])
+    # https://github.com/rails/rails/blob/4-0-stable/activerecord/lib/active_record/connection_adapters/connection_specification.rb#L68-L92
+    # ActiveRecord::ConnectionAdapters::ConnectionSpecification::ConnectionUrlResolver.new(url).to_hash
+    # https://github.com/rails/rails/blob/4-1-stable/activerecord/lib/active_record/connection_adapters/connection_specification.rb#L17-L121
+    # ActiveRecord::ConnectionHandling::MergeAndResolveDefaultUrlConfig.new(url_config).resolve
+    # ActiveRecord::ConnectionAdapters::ConnectionSpecification::ConnectionUrlResolver.new(url).to_hash
+    # https://github.com/rails/rails/blob/4-2-stable/activerecord/lib/active_record/connection_handling.rb#L60-L81
+    # https://github.com/rails/rails/blob/97b980b4e61aea3cee429bdee4b2eae2329905cd/activerecord/lib/active_record/connection_handling.rb#L60-L81
+    #
+    # Expands a connection string into a hash.
+    class ConnectionUrlResolver # :nodoc:
+
+      # == Example
+      #
+      #   url = "postgresql://foo:bar@localhost:9000/foo_test?pool=5&timeout=3000"
+      #   ConnectionUrlResolver.new(url).to_hash
+      #   # => {
+      #     "adapter"  => "postgresql",
+      #     "host"     => "localhost",
+      #     "port"     => 9000,
+      #     "database" => "foo_test",
+      #     "username" => "foo",
+      #     "password" => "bar",
+      #     "pool"     => "5",
+      #     "timeout"  => "3000"
+      #   }
+      def initialize(url)
+        raise "Database URL cannot be empty" if url.blank?
+        @uri     = uri_parser.parse(url)
+        @adapter = @uri.scheme.tr('-', '_')
+        @adapter = "postgresql" if @adapter == "postgres"
+
+        if @uri.opaque
+          @uri.opaque, @query = @uri.opaque.split('?', 2)
+        else
+          @query = @uri.query
+        end
+      end
+
+      # Converts the given URL to a full connection hash.
+      def to_hash
+        config = raw_config.reject { |_,value| value.blank? }
+        config.map { |key,value| config[key] = uri_parser.unescape(value) if value.is_a? String }
+        config
+      end
+
+      private
+
+      def uri
+        @uri
+      end
+
+      def uri_parser
+        @uri_parser ||= URI::Parser.new
+      end
+
+      # Converts the query parameters of the URI into a hash.
+      #
+      #   "localhost?pool=5&reaping_frequency=2"
+      #   # => { "pool" => "5", "reaping_frequency" => "2" }
+      #
+      # returns empty hash if no query present.
+      #
+      #   "localhost"
+      #   # => {}
+      def query_hash
+        Hash[(@query || '').split("&").map { |pair| pair.split("=") }]
+      end
+
+      def raw_config
+        if uri.opaque
+          query_hash.merge({
+            "adapter"  => @adapter,
+            "database" => uri.opaque })
+        else
+          query_hash.merge({
+            "adapter"  => @adapter,
+            "username" => uri.user,
+            "password" => uri.password,
+            "port"     => uri.port,
+            "database" => database_from_path,
+            "host"     => uri.hostname })
+        end
+      end
+
+      # Returns name of the database.
+      def database_from_path
+        if @adapter == 'sqlite3'
+          # 'sqlite3:/foo' is absolute, because that makes sense. The
+          # corresponding relative version, 'sqlite3:foo', is handled
+          # elsewhere, as an "opaque".
+
+          uri.path
+        else
+          # Only SQLite uses a filename as the "database" name; for
+          # anything else, a leading slash would be silly.
+
+          uri.path.sub(%r{^/}, "")
+        end
+      end
+    end
+
     # NOTE: url format must be, e.g.
     # url: mysql2://...
     # NOT
@@ -34,25 +142,12 @@ module Makara
     # NOTE: Does not use ENV['DATABASE_URL']
     def self.merge_and_resolve_default_url_config(config)
       return config unless config.key?(:url)
-      url_config = config.slice(:url)
-      url_config = connection_url_to_hash(url_config)
-      url_config = url_config[:url].symbolize_keys
+      url = config[:url]
+      url_config = ConnectionUrlResolver.new(url).to_hash
+      url_config = url_config.symbolize_keys
       url_config.delete(:adapter)
       config.delete(:url)
       config.update(url_config)
-    end
-
-    def self.connection_url_to_hash(url_config)
-      if defined?(ActiveRecord::ConnectionHandling::MergeAndResolveDefaultUrlConfig)
-        ActiveRecord::ConnectionHandling::MergeAndResolveDefaultUrlConfig.new(url_config).resolve
-      elsif defined?(ActiveRecord::Core::ConnectionSpecification::Resolver)
-        {
-          :url =>
-          ActiveRecord::Core::ConnectionSpecification::Resolver.send(:connection_url_to_hash, url_config[:url])
-        }
-      else
-        fail 'Unknown connection_url_hash method'
-      end
     end
 
     attr_reader :makara_config
