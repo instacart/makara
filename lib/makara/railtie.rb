89afdd4ce58d8128635cdcc609c47fc9bcb4e542
Joshua Gross <joshua@surfeasy.com>
2016-09-12 13:49:59 -0400

    comment out railtie test

--- lib/makara/railtie.rb
+++ lib/makara/railtie.rb
@@ -1,15 +1,15 @@
 module Makara
   class Railtie < ::Rails::Railtie
 
-    initializer "makara.configure_rails_initialization" do |app|
-      app.middleware.use Makara::Middleware
-    end
-
-    initializer "makara.initialize_logger" do |app|
-      ActiveRecord::LogSubscriber.log_subscribers.each do |subscriber|
-        subscriber.extend ::Makara::Logging::Subscriber
-      end
-    end
+    #initializer "makara.configure_rails_initialization" do |app|
+    #  app.middleware.use Makara::Middleware
+    #end
+#
+    #initializer "makara.initialize_logger" do |app|
+    #  ActiveRecord::LogSubscriber.log_subscribers.each do |subscriber|
+    #    subscriber.extend ::Makara::Logging::Subscriber
+    #  end
+    #end
 
   end
 end
