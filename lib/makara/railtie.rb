c14102a2e5f28be28c8cb64b4a2c5583f3887e62
Joshua Gross <joshua@surfeasy.com>
2016-09-12 14:10:23 -0400

    more debuggers

--- lib/makara/railtie.rb
+++ lib/makara/railtie.rb
@@ -1,14 +1,15 @@
 module Makara
   class Railtie < ::Rails::Railtie
 
-    debugger
-    
+    require 'byebug'; byebug
+
     initializer "makara.configure_rails_initialization" do |app|
-      debugger
+      require 'byebug'; byebug
       app.middleware.use Makara::Middleware
     end
 
     initializer "makara.initialize_logger" do |app|
+      require 'byebug'; byebug
       ActiveRecord::LogSubscriber.log_subscribers.each do |subscriber|
         subscriber.extend ::Makara::Logging::Subscriber
       end
