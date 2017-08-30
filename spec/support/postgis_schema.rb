23129bc103f0738c9986e163b241111a2cba2c88
Brian Leonard <brian@bleonard.com>
2017-08-30 15:36:49 -0700

    continued weirdness in test tables

--- spec/support/postgis_schema.rb
+++ spec/support/postgis_schema.rb
@@ -2,6 +2,12 @@
 ActiveRecord::Schema.define(:version => 20160518161227) do
   execute "create extension if not exists postgis"
 
+  begin
+    drop_table "towns" if table_exists? "towns"
+  rescue Exception => e
+    puts "#{e.message}\n\t#{e.backtrace.join("\n\t")}"
+  end
+
   create_table "towns", :force => true do |t|
     t.st_point "location"
   end
--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -1,6 +1,10 @@
 ActiveRecord::Schema.define(:version => 20130628161227) do
 
-  drop_table "users" if table_exists? "users"
+  begin
+    drop_table "users" if table_exists? "users"
+  rescue Exception => e
+    puts "#{e.message}\n\t#{e.backtrace.join("\n\t")}"
+  end
 
   create_table "users" do |t|
     t.string   "name"
