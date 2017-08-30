074ff34aea9532930fb3d2faeefd262bee46b705
Brian Leonard <brian@bleonard.com>
2017-08-30 16:14:47 -0700

    try to truncate instead

--- spec/support/postgis_schema.rb
+++ spec/support/postgis_schema.rb
@@ -2,13 +2,13 @@
 ActiveRecord::Schema.define(:version => 20160518161227) do
   execute "create extension if not exists postgis"
 
-  begin
-    drop_table "towns" if table_exists? "towns"
-  rescue Exception => e
-    puts "#{e.message}\n\t#{e.backtrace.join("\n\t")}"
+  if table_exists? "towns"
+    ActiveRecord::Base.connection.execute("TRUNCATE TABLE towns")
+  else
+    create_table "towns", :force => true do |t|
+      t.st_point "location"
+    end
   end
 
-  create_table "towns", :force => true do |t|
-    t.st_point "location"
-  end
+
 end
--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -1,13 +1,11 @@
 ActiveRecord::Schema.define(:version => 20130628161227) do
 
-  begin
-    drop_table "users" if table_exists? "users"
-  rescue Exception => e
-    puts "#{e.message}\n\t#{e.backtrace.join("\n\t")}"
-  end
-
-  create_table "users" do |t|
-    t.string   "name"
+  if table_exists? "users"
+    ActiveRecord::Base.connection.execute("TRUNCATE TABLE users")
+  else
+    create_table "users" do |t|
+      t.string   "name"
+    end
   end
 
 end
