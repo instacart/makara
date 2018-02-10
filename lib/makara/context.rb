78c285c2e97344eadf0674ee8735193fe71294af
Rosa Gutierrez <rosa.ge@gmail.com>
2018-02-10 14:55:48 +0100

    Convert Context into a stickiness state representation
    Instead of keeping track of two MD5 hashes that allow us to be stuck to
    master for 1 or 2 requests at most, keep whether we are currently stuck
    or not, for each configuration.
    
    The context represents the current stickiness state. It knows whether we
    are stuck to master for each of the different Makara configurations we
    have defined. It's persisted between requests via an only cookie we set,
    whose max age depends on stickiness duration for each of the
    configurations we store at any given moment.

--- lib/makara/context.rb
+++ lib/makara/context.rb
@@ -1,83 +1,100 @@
 require 'digest/md5'
 
-# Keeps track of the current and previous context (hexdigests)
-# If a new context is needed it can be generated via Makara::Context.generate
-
+# Keeps track of the current stickiness state for different Makara configurations
 module Makara
   class Context
+    attr_accessor :data
+
+    IDENTIFIER = '_mkra_ctxt'
+
+    DEFAULT_OPTIONS = {
+      path: "/",
+      http_only: true
+    }
+
+    def initialize(data)
+      @data = data
+      @dirty = false
+    end
+
+    def stick(config_id, ttl)
+      data[config_id] = Time.now.to_f + ttl.to_f
+      @dirty = true
+    end
+
+    def stuck?(config_id)
+      data[config_id] && data[config_id] > Time.now.to_f
+    end
+
+    # Indicates whether there have been changes to the context that need
+    # to be persisted when the request finishes
+    def dirty?
+      @dirty
+    end
+
+    def to_cookie_options
+      max_age = (data.values.max - Time.now.to_f).ceil + 1
+      value = data.collect { |config_id, ttl| "#{config_id}:#{ttl}" }.join('|')
+      { :max_age => max_age, :value => value }
+    end
+
     class << self
-
-      def generate(seed = nil)
-        seed ||= "#{Time.now.to_i}#{Thread.current.object_id}#{rand(99999)}"
-        Digest::MD5.hexdigest(seed)
-      end
-
-      def get_previous
-        fetch(:makara_context_previous) { generate }
-      end
-
-      def set_previous(context)
-        previously_sticky.clear
-        set(:makara_context_previous,context)
-      end
-
-      def get_current
-        fetch(:makara_context_current) { generate }
-      end
-
-      def set_current(context)
-        set(:makara_context_current,context)
-      end
-
-      def previously_stuck?(config_id)
-        previously_sticky.fetch(config_id) do
-          stuck?(Makara::Context.get_previous, config_id)
-        end
+      def init(request)
+        data = parse(request.cookies[IDENTIFIER].to_s)
+        set(:makara_current_context, new(data))
       end
 
       # Called by `Proxy#stick_to_master!` to stick subsequent requests to
-      # master. They'll see the current context as their previous context
-      # when they're asking whether they should be stuck to master.
-      def stick(context, config_id, ttl)
-        Makara::Cache.write(cache_key_for(context, config_id), '1', ttl)
+      # master when using the given config
+      def stick(config_id, ttl)
+        current.stick(config_id, ttl)
       end
 
-      def stuck?(context, config_id)
-        !!Makara::Cache.read(cache_key_for(context, config_id))
+      def stuck?(config_id)
+        current.stuck?(config_id)
+      end
+
+      def commit(headers)
+        if current.dirty?
+          Rack::Utils.set_cookie_header! headers, IDENTIFIER, DEFAULT_OPTIONS.merge(current.to_cookie_options)
+        end
       end
 
       protected
-
-      def previously_sticky
-        fetch(:makara_previously_sticky) { Hash.new }
-      end
-
-      def cache_key_for(context, config_id)
-        "makara::#{context}-#{config_id}"
-      end
-
-      def fetch(key)
-        get(key) || set(key,yield)
-      end
-
-      if Thread.current.respond_to?(:thread_variable_get)
-        def get(key)
-          Thread.current.thread_variable_get(key)
+        def current
+          get(:makara_current_context)
         end
 
-        def set(key,value)
-          Thread.current.thread_variable_set(key,value)
-        end
-      else
-        def get(key)
-          Thread.current[key]
+        if Thread.current.respond_to?(:thread_variable_get)
+          def get(key)
+            Thread.current.thread_variable_get(key)
+          end
+
+          def set(key, value)
+            Thread.current.thread_variable_set(key,value)
+          end
+        else
+          def get(key)
+            Thread.current[key]
+          end
+
+          def set(key, value)
+            Thread.current[key]=value
+          end
         end
 
-        def set(key,value)
-          Thread.current[key]=value
-        end
-      end
+      private
+        # Pairs of {config_id}:{timestamp}, separated by "|"
+        # config_id1:1518270031.3132212|config_id2:1518270030.313232 ..
+        def parse(cookie_string)
+          return {} if cookie_string.empty?
 
+          states = cookie_string.split("|")
+          states.each_with_object({}) do |state, data|
+            config_id, timestamp = state.split(":")
+            data[config_id] = timestamp.to_f if config_id && timestamp
+          end
+        end
     end
   end
 end
