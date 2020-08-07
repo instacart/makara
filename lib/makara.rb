8b47e6c6e8c24a99a9cf2afbbaf47c17b89357bc
Marco Montagna <marco.montagna@instacart.com>
2020-08-07 11:23:01 -0700

    Fix autoload.

--- lib/makara.rb
+++ lib/makara.rb
@@ -17,6 +17,7 @@ module Makara
     autoload :MakaraError,                'makara/errors/makara_error'
     autoload :AllConnectionsBlacklisted,  'makara/errors/all_connections_blacklisted'
     autoload :BlacklistConnection,        'makara/errors/blacklist_connection'
+    autoload :BlacklistConnectionOnMaster,     'makara/errors/blacklist_connection_on_master'
     autoload :NoConnectionsAvailable,     'makara/errors/no_connections_available'
   end
 
