b53800d0157a67d7ac6654424ba3e4d1cbd0aff7
Matt Larraz <mlarraz@users.noreply.github.com>
2021-01-19 11:36:47 -0800

    Fix on Ruby 3 (#283)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,17 @@ begin
 rescue LoadError
 end
 
+if RUBY_VERSION >= "2.7.0"
+  Warning[:deprecated] = true
+end
+
+# Delete once Timecop fixes Ruby 3.1 support
+Time.class_eval do
+  class << self
+    ruby2_keywords :new if Module.private_method_defined?(:ruby2_keywords)
+  end
+end
+
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
