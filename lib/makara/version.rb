8e48d797a41721bcc597f7350f94be77b707abcb
Matt Larraz <mlarraz@users.noreply.github.com>
2021-02-05 12:10:22 -0500

    Tick v0.6.0 pre-release

--- lib/makara/version.rb
+++ lib/makara/version.rb
@@ -1,9 +1,9 @@
 module Makara
   module VERSION
     MAJOR = 0
-    MINOR = 5
+    MINOR = 6
     PATCH = 0
-    PRE = nil
+    PRE = "pre"
 
     def self.to_s
       [MAJOR, MINOR, PATCH, PRE].compact.join('.')
